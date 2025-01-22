5a1d8b91d815d1f41a92d320af6c6edb3357ff8c
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-01-23 00:06:07 +0800

    adds the rustdoc generated html for linked_list (data_structure)

---
+++ data_structure/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["data_structure"];
+//{"start":21,"fragment_lengths":[16]}
---
+++ data_structure/doc/trait.impl/core/clone/trait.Clone.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["data_structure",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"data_structure/linked_list/struct.ListNode.html\" title=\"struct data_structure::linked_list::ListNode\">ListNode</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[465]}
