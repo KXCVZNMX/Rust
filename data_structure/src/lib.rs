44d34bf0afdce386388d3bd7298e48b894a6202c
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-03-22 17:10:40 +1100

    Finished the Stack implementation with doctest and comments

--- data_structure/src/lib.rs
+++ data_structure/src/lib.rs
@@ -574,9 +574,8 @@ pub mod ds {
     /// * [new](struct.Stack.html#method.new) -> `Self`
     /// * [from_vec](struct.Stack.html#method.from_vec) -> `Self`
     /// * [print](struct.Stack.html#method.print) -> `()`
-    /// * [pop](struct.Stack.html#method.pop) -> `()`
+    /// * [pop](struct.Stack.html#method.pop) -> `T`
     /// * [push](struct.Stack.html#method.push) -> `()`
-    /// * [is_empty](struct.Stack.html#method.is_empty) -> `bool`
     /// * [peak](struct.Stack.html#method.peak) -> `T`
     /// * [clear](struct.Stack.html#method.clear) -> `()`
     pub mod stack {
@@ -593,12 +592,40 @@ pub mod ds {
         where T:
             Display
         {
+            /// Constructs a new instance of `Stack<T>` with the provided
+            /// `val: T`, returning `Self`
+            ///
+            /// # Example
+            /// ```
+            /// # use crate::data_structure::ds::stack::Stack;
+            /// # use crate::data_structure::ds::linked_list::ListNode;
+            /// let stack = Stack::new(1);
+            /// assert_eq!(stack, Stack{list: ListNode::new(1), len: 1});
+            /// ```
+            ///
+            /// There cannot be an empty stack if you are initialising it.
             pub fn new(val: T) -> Self {
                 Stack {
                     list: ListNode::new(val),
                     len: 1
                 }
             }
+
+            /// Constructs a new instance of `Stack<T>` with the provided
+            /// `Vec<T>`, returning `Self`
+            ///
+            /// # Example
+            /// ```
+            /// # use crate::data_structure::ds::{stack::Stack, linked_list::ListNode};
+            /// let stack = Stack::from_vec(vec![1, 2, 3]);
+            /// assert_eq!(
+            ///     stack,
+            ///     Stack{
+            ///         len: 3,
+            ///         list: ListNode::from_vec(vec![1, 2, 3]),
+            ///     }
+            /// )
+            /// ```
             pub fn from_vec(vec: Vec<T>) -> Self
             where T:
                 Clone
@@ -610,13 +637,26 @@ pub mod ds {
                 }
             }
 
+            /// Prints the given Stack.
+            ///
+            /// # Example Output
+            ///
+            /// ```text
+            /// +---+---+---+---+---+
+            /// | 1 | 2 | 3 | 4 | 5 |
+            /// +---+---+---+---+---+
+            ///   ↑
+            ///  HEAD
+            /// ```
+            ///
+            /// The stack is displayed with the top (HEAD) indicated.
             pub fn print(&self) {
                 let separator = "+---".repeat(self.len) + "+";
 
                 println!("{}", separator);
 
                 for i in 0..self.len {
-                    print!("| {:^2}", self.list[i]); // Center align numbers in a 3-width space
+                    print!("| {:^2}", self.list[i]);
                 }
 
                 println!("|");
@@ -625,11 +665,30 @@ pub mod ds {
                 println!(" HEAD");
             }
 
-            pub fn pop(&mut self) {
-                self.list.pop();
+            /// Pops the first element of the stack off, returning it
+            ///
+            /// # Example
+            /// ```
+            /// # use crate::data_structure::ds::stack::Stack;
+            /// let mut stack = Stack::from_vec(vec![1, 1, 2, 3, 4, 5]);
+            /// let poped_val = stack.pop();
+            /// assert_eq!(poped_val, 1);
+            /// ```
+            pub fn pop(&mut self) -> T {
+                let val = self.list.pop().unwrap();
                 self.len -= 1;
+                val
             }
 
+            /// Pushes an element on to the top of the stack
+            ///
+            /// # Example
+            /// ```
+            /// # use crate::data_structure::ds::stack::Stack;
+            /// let mut stack = Stack::from_vec(vec![2, 3, 4, 5]);
+            /// stack.push(1);
+            /// assert_eq!(stack, Stack::from_vec(vec![1, 2, 3, 4, 5]));
+            /// ```
             pub fn push(&mut self, val: T)
             where T:
                 Clone
@@ -638,10 +697,16 @@ pub mod ds {
                 self.len += 1;
             }
 
-            pub fn is_empty(&self) -> bool {
-                self.len == 0
-            }
-
+            /// Clears the entire stack, making it with one element with value of `T::default`
+            ///
+            /// # Example
+            /// ```
+            /// # use crate::data_structure::ds::{linked_list::ListNode, stack::Stack};
+            ///
+            /// let mut stack = Stack::from_vec(vec![1, 2, 3, 4, 5]);
+            /// stack.clear();
+            /// assert_eq!(stack, Stack{len: 1, list: ListNode::new(i32::default())});
+            /// ```
             pub fn clear(&mut self)
             where T:
                 Default
@@ -650,11 +715,37 @@ pub mod ds {
                 self.len = 1;
             }
 
+            /// Peaks at the top of the stack, returning the value `T`, but not removing it
+            ///
+            /// # Example
+            /// ```
+            /// # use crate::data_structure::ds::stack::Stack;
+            /// let stack = Stack::from_vec(vec![1, 2, 3, 4, 5]);
+            /// let peak_val = stack.peak();
+            /// assert_eq!(peak_val, 1);
+            /// assert_eq!(stack, Stack::from_vec(vec![1, 2, 3, 4, 5]));
+            /// ```
             pub fn peak(&self) -> T
             where T:
                 Clone
             {
                 self.list.val.clone()
+            }
+        }
+
+        impl<T: PartialEq + Clone + Copy> PartialEq for Stack<T> {
+            fn eq(&self, other: &Self) -> bool {
+                if self.len != other.len {
+                    return false;
+                }
+                let head = *self.list.copy();
+                let other_head = *self.list.copy();
+                for i in 0..self.len {
+                    if head[i] != other_head[i] {
+                        return false;
+                    }
+                }
+                true
             }
         }
     }
